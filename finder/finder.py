c8a528c632db3b03da4f5e72c72dccf17f5f6f83
jocelynyuan <jocelyn19950627@yahoo.com.tw>
2021-06-12 13:44:49 +0900

    find with city name

---
+++ finder/finder.py
@@ -0,0 +1,18 @@
+
+import sys
+import urllib.parse
+import requests
+
+BASE_URI = "www.refugerestrooms.org/api"
+def search_toilet(query):
+    url = f"https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=10&offset=0&query={query}"
+    response = requests.get(url).json()
+    if  len(response) > 0 :
+        street = response[0]["name"]
+        return street
+    return "no toilets found"
+
+if __name__ == "__main__":
+    query = "tokyo"
+    street = search_toilet(query)
+    print(street)
